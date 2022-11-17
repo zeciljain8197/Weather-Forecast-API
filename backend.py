59bf5862f59f189d4959de241492444091a82ab7
Zecil Jain <zecil.jain@gmail.com>
2022-11-17 09:20:26 +0530

    Update weather forecast repository

---
+++ backend.py
@@ -0,0 +1,13 @@
+import requests
+
+API_KEY = "3a29432db958616c6baa068ec5fb5b0b"
+
+
+def get_data(place, forecast_days=None):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
